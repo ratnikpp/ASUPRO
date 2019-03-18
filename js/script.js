b765852c6cf7e669e524c8ca74317330650d8654
Ratnikov Pavel <39724877+ratnikpp@users.noreply.github.com>
2019-03-18 15:10:49 +0300

    1819
    1819

--- js/script.js
+++ js/script.js
@@ -41,3 +41,18 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+
+
+//carousell products
+$(function(){
+  $('.crsl-items').carousel({
+    visible: 3,
+    itemMinWidth: 180,
+    itemEqualHeight: 370,
+    itemMargin: 9,
+  });
+  
+  $("a[href=#]").on('click', function(e) {
+    e.preventDefault();
+  });
+});
