074069e6e59724a3f3ad1791f27760c379510f95
Ratnikov Pavel <39724877+ratnikpp@users.noreply.github.com>
2019-05-20 16:33:55 +0300

    fd
    dfd

--- js/script.js
+++ js/script.js
@@ -50,3 +50,22 @@ $(function(){
     e.preventDefault();
   });
 });
+
+
+function bacToTop() {
+  let button = $('.back-to-top');
+  $(window).on('scroll', () => {
+    if ($(this).scrollTop() >= 50){
+    button.fadeIn();
+  } else {
+    button.fadeOut();
+  }
+});
+
+  button.on('click', (e) => {
+    e.preventDefault();
+    $('html').animate({scrollTop: 0}, 1000);
+  })
+}
+
+backToTop();
